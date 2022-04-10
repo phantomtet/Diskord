4f40b063152b004fb612d54ad68be183da9928e6
phantomtet <44901513+phantomtet@users.noreply.github.com>
2022-04-10 13:40:00 +0700

    u

--- client/src/components/layout/Sidebar.component.js
+++ client/src/components/layout/Sidebar.component.js
@@ -19,7 +19,7 @@ const Sidebar = () => {
             backgroundColor: '#202225'
         }}
         >
-            Sidebar
+            Sidebar1
             <Button onClick={() => history.push('/')}>/</Button>
             <Button onClick={() => history.push('/@me')}>me</Button>
             <Button onClick={handleLogout}>Log out</Button>
